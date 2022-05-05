405dc9ff4a0439ebddad1983fa31d5d99c99f594
Otsuka, Kohei <otsuka_kunikura@outlook.com>
2022-05-05 18:52:29 +0900

    Implement Rhinoroid class

--- rhinoroid/src/main/java/com/github/otsuka_kohei/rhinoroid/Rhinoroid.kt
+++ rhinoroid/src/main/java/com/github/otsuka_kohei/rhinoroid/Rhinoroid.kt
@@ -1,4 +1,37 @@
 package com.github.otsuka_kohei.rhinoroid
 
-class Rhinoroid {
+import android.content.Context
+import org.mozilla.javascript.Scriptable
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.SequenceInputStream
+import java.util.*
+
+
+class Rhinoroid(private val context: Context) {
+
+    private val rhinoContext: org.mozilla.javascript.Context =
+        org.mozilla.javascript.Context.enter()
+    private val scriptable: Scriptable = rhinoContext.initStandardObjects()
+
+    private val jsInputStreamList = ArrayList<InputStream>()
+
+    init {
+        rhinoContext.optimizationLevel = -1
+    }
+
+    fun import(jsFilePathInAssetFolder: String) {
+        val inputStream: InputStream = context.assets.open(jsFilePathInAssetFolder)
+        jsInputStreamList.add(inputStream)
+    }
+
+    fun evaluate(js: String): Any {
+        val jsInputStream = ByteArrayInputStream(js.toByteArray())
+        jsInputStreamList.add(jsInputStream)
+        val concatenatedImportedJsInputStream =
+            SequenceInputStream(Collections.enumeration(jsInputStreamList))
+        val inputStreamReader = InputStreamReader(concatenatedImportedJsInputStream)
+        return rhinoContext.evaluateReader(scriptable, inputStreamReader, "js", 0, null)
+    }
 }
