5d3d92913ef4d6961fdf751bb8cdd7e0dd4d0909
Otsuka, Kohei <otsuka_kunikura@outlook.com>
2022-05-05 21:26:28 +0900

    Add comment

--- rhinoroid/src/main/java/com/github/otsuka_kohei/rhinoroid/Rhinoroid.kt
+++ rhinoroid/src/main/java/com/github/otsuka_kohei/rhinoroid/Rhinoroid.kt
@@ -9,6 +9,13 @@ import java.io.SequenceInputStream
 import java.util.*
 
 
+/**
+ * Evaluate JavaScript on Android.
+ * This class supports importing JavaScript (.js) file from asset folder.
+ * Import method works like HTML tag <script src="hoge.js"></script>.
+ *
+ * @property context
+ */
 class Rhinoroid(private val context: Context) {
 
     private val rhinoContext: org.mozilla.javascript.Context =
@@ -21,11 +28,27 @@ class Rhinoroid(private val context: Context) {
         rhinoContext.optimizationLevel = -1
     }
 
+
+    /**
+     * Import JavaScript (.js) file from asset folder.
+     * This method works like HTML tag <script src="hoge.js"></script>.
+     * If import JavaScript (.js) file from .../assets/hoge/fuga.js, pass a string "hoge/fuga.js".
+     *
+     * @param jsFilePathInAssetFolder Path of JavaScript (.js) file in asset folder.
+     */
     fun import(jsFilePathInAssetFolder: String) {
         val inputStream: InputStream = context.assets.open(jsFilePathInAssetFolder)
         jsInputStreamList.add(inputStream)
     }
 
+
+    /**
+     * Evaluate given JavaScript code.
+     * If you call import() method before, evaluation can refer imported JavaScript (.js) file.
+     *
+     * @param js JavaScript code which you want to evaluate.
+     * @return Result of evaluation.
+     */
     fun evaluate(js: String): Any {
         val jsInputStream = ByteArrayInputStream(js.toByteArray())
         jsInputStreamList.add(jsInputStream)
