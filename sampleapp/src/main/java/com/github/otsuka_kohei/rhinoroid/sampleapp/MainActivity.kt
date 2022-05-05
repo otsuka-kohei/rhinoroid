349642525cd8b3762975f65ccdde210d97599143
Otsuka, Kohei <otsuka_kunikura@outlook.com>
2022-05-05 18:52:57 +0900

    Update sample app

--- sampleapp/src/main/java/com/github/otsuka_kohei/rhinoroid/sampleapp/MainActivity.kt
+++ sampleapp/src/main/java/com/github/otsuka_kohei/rhinoroid/sampleapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.github.otsuka_kohei.rhinoroid.sampleapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.github.otsuka_kohei.rhinoroid.Rhinoroid
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rhinoroid = Rhinoroid(this)
+        rhinoroid.import("a.js")
+        rhinoroid.import("b.js")
+        rhinoroid.import("c/c.js")
+        val result = rhinoroid.evaluate("funcA() + funcB() + funcC();") as String
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = result
     }
 }
